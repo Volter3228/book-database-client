0cb0caf24932f3468611c36569d1d8e7d6c2bda4
Volter3228 <vova.morozov3228@gmail.com>
2021-08-25 18:12:58 +0300

    Update request endpoints

--- src/store/book/actions.js
+++ src/store/book/actions.js
@@ -1,7 +1,7 @@
 import { api } from 'boot/axios';
 
 export const getBooks = async ({ commit }) => {
-  const response = await api.get('/getBooks');
+  const response = await api.get('/books');
   commit('SET_BOOKS', response.data);
   return response.data;
 }
--- src/store/place/actions.js
+++ src/store/place/actions.js
@@ -1,7 +1,7 @@
 import { api } from 'boot/axios';
 
 export const getPlaces = async ({ commit }) => {
-  const response = await api.get('/getPlaces');
+  const response = await api.get('/places');
   commit('SET_PLACES', response.data);
   return response.data;
 };
