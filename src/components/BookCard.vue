80d9dd1e3359c349a7d6117cb9940dd047a44267
Volter3228 <vova.morozov3228@gmail.com>
2021-08-25 18:06:56 +0300

    Update Book Card view

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -18,18 +18,18 @@
     </transition>
     <q-card-section class="full-width">
       <div class="text-h6">{{ book.title }}</div>
-      <div class="text-subtitle2">
+      <div class="text-subtitle2 full-width text-right text-italic">
         by {{ book.authors.map(a => a.name).join(', ') }}
-      </div>
-      <div class="text-caption text-italic">
-        {{ book.topics.map(a => a.name).join(', ') }}
       </div>
     </q-card-section>
 
-    <q-separator dark inset />
-
     <q-card-section class="q-pt-none">
-      {{ book.description }}
+      <div>
+        <strong>Topics:</strong> {{ book.topics.map(a => a.name).join(', ') }}
+      </div>
+      <div>
+        <strong>Description:</strong> {{ book.description }}
+      </div>
     </q-card-section>
   </q-card>
 </template>
