818e28d05e2fc7b8a6fc3d493a451c0b427d3b39
Volter3228 <vova.morozov3228@gmail.com>
2021-08-25 09:40:13 +0300

    Create Places tab

---
+++ src/components/PlacesTab.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="places-tab flex justify-center">
+    <q-scroll-area class="full-width q-px-lg" style="height: 70vh">
+      <div class="row">
+        <div class="col-xs-6 col-sm-4 col-md-3 q-pa-sm" v-for="place in places" :key="place.id">
+          <q-card
+            class="place-card full-width"
+          >
+            <q-img
+              :src="place.type === 'Bookcase' ? 'images/bookcase.jpg' : 'images/box.jpg'"
+              :ratio="16/9"
+            />
+            <q-card-section>
+              <div class="text-h6">{{ place.type }}</div>
+              <div class="text-subtitle2">{{ place.booksCount }} {{ place.booksCount > 1 ? 'books' : 'book' }} </div>
+            </q-card-section>
+
+            <q-card-section class="q-pt-none">
+              {{ place.info }}
+            </q-card-section>
+          </q-card>
+        </div>
+
+      </div>
+
+    </q-scroll-area>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlacesTab',
+  setup() {
+    const places = [
+      { id: 1, type: 'Bookcase', shelvesCount: 4, booksCount: 12, info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consectetur' },
+      { id: 2, type: 'Box', booksCount: 5, info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consectetur' },
+      { id: 3, type: 'Box', booksCount: 8, info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consectetur' },
+      { id: 4, type: 'Bookcase', shelvesCount: 4, booksCount: 20, info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consectetur' },
+      { id: 5, type: 'Bookcase', shelvesCount: 4, booksCount: 12, info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consectetur' },
+      { id: 6, type: 'Box', booksCount: 5, info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consectetur' },
+      { id: 7, type: 'Box', booksCount: 8, info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consectetur' },
+      { id: 8, type: 'Bookcase', shelvesCount: 4, booksCount: 20, info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consectetur' },
+    ];
+
+    return {
+      places
+    }
+  }
+}
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,42 @@
 <template>
-  <q-page class="flex justify-center q-pa-xl bg-brown-1">
-    <div class="flex full-width bg-brown-7 rounded-borders">
+  <q-page class="flex justify-center q-pa-xl q-pb-md bg-brown-1">
+    <div class="flex column full-width bg-brown-7 rounded-borders shadow-5">
+      <q-tabs
+        v-model="tab"
+        inline-label
+        class="bg-brown-5 text-white shadow-2 rounded-borders"
+      >
+        <q-tab name="books" icon="library_books" label="Books"/>
+        <q-tab name="places" icon="other_houses" label="Places"/>
+      </q-tabs>
 
+      <q-separator />
+
+      <q-tab-panels v-model="tab" animated style="background: inherit">
+        <q-tab-panel name="books">
+
+        </q-tab-panel>
+        <q-tab-panel name="places">
+          <PlacesTab />
+        </q-tab-panel>
+      </q-tab-panels>
     </div>
   </q-page>
 </template>
 
 <script>
+import {ref} from 'vue';
+import PlacesTab from "components/PlacesTab";
+
 export default {
-  name: 'MainPage'
+  name: 'MainPage',
+  components: {PlacesTab},
+  setup() {
+    const tab = ref('books');
+
+    return {
+      tab
+    }
+  }
 }
 </script>
