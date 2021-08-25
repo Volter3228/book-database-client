8aa65ed15e33c68a8908a6076a17cae5da520ba1
Volter3228 <vova.morozov3228@gmail.com>
2021-08-25 19:23:47 +0300

    Update Header Toolbar

--- src/components/HeaderToolbar.vue
+++ src/components/HeaderToolbar.vue
@@ -1,31 +1,29 @@
 <template>
   <q-toolbar>
+
     <q-toolbar-title>
       <q-avatar square class="q-mr-sm">
         <img src="icons/favicon-128x128.png" alt="logo"/>
       </q-avatar>
       <strong>Book Database</strong>
     </q-toolbar-title>
+
     <q-space/>
+
     <Search/>
-    <q-btn flat round dense icon="book">
-      <q-tooltip anchor="bottom middle" self="top middle" :offset="[5, 3]" class="bg-brown-9">
-        <strong>Add new book</strong>
-      </q-tooltip>
-    </q-btn>
-    <q-btn flat round dense icon="inventory_2">
-      <q-tooltip anchor="bottom middle" self="top middle" :offset="[5, 3]" class="bg-brown-9">
-        <strong>Add new place</strong>
-      </q-tooltip>
-    </q-btn>
+    <AddBookDialogBtn />
+    <AddPlaceDialogBtn />
+
   </q-toolbar>
 </template>
 
 <script>
 import Search from "components/Search";
+import AddBookDialogBtn from "components/AddBookDialogBtn";
+import AddPlaceDialogBtn from "components/AddPlaceDialogBtn";
 
 export default {
   name: 'HeaderToolbar',
-  components: {Search}
+  components: {AddPlaceDialogBtn, AddBookDialogBtn, Search}
 }
 </script>
