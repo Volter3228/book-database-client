12d33412aba851b5a4563ec900c39505e58f99d7
Volter3228 <vova.morozov3228@gmail.com>
2021-08-26 04:01:43 +0300

    Add try catch block to book actions

--- src/store/book/actions.js
+++ src/store/book/actions.js
@@ -1,8 +1,11 @@
 import { api } from 'boot/axios';
 
 export const getBooks = async ({ commit }) => {
-  const response = await api.get('/books');
-  commit('SET_BOOKS', response.data);
-  return response.data;
+  try {
+    const response = await api.get('/books');
+    commit('SET_BOOKS', response.data);
+    return response.data;
+  } catch(err) { console.log(err) }
 }
+
 
