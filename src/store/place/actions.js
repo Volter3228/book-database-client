d0d5c1e66912f33213e2020ebaf743f9bc8ad501
Volter3228 <vova.morozov3228@gmail.com>
2021-08-26 04:01:07 +0300

    Update places store actions and mutations

--- src/store/place/actions.js
+++ src/store/place/actions.js
@@ -1,8 +1,19 @@
 import { api } from 'boot/axios';
+import { Notify } from 'quasar';
 
 export const getPlaces = async ({ commit }) => {
-  const response = await api.get('/places');
-  commit('SET_PLACES', response.data);
-  return response.data;
+  try {
+    const response = await api.get('/places');
+    commit('SET_PLACES', response.data);
+    return response.data;
+  } catch(err) { console.log(err) }
 };
 
+export const addPlace = async ({ commit }, data) => {
+  try {
+    const response = await api.post('/places', data);
+    commit('ADD_PLACE', response.data);
+    Notify.create({ message: 'Place Added Successfully!', color: 'positive' });
+    return response.data;
+  } catch(err) { Notify.create({ message: err.response.data.message, color: 'negative' }) }
+}
--- src/store/place/mutations.js
+++ src/store/place/mutations.js
@@ -2,3 +2,7 @@ export const SET_PLACES = (state, payload) => {
   state.places = payload;
 }
 
+export const ADD_PLACE = (state, payload) => {
+  state.places.unshift(payload);
+}
+
