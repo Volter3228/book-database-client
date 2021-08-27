dcb8fbf840c2da33d1a41ec19e405b7211afc5bb
Volter3228 <vova.morozov3228@gmail.com>
2021-08-27 15:09:46 +0300

    Create books search

--- src/components/BooksTab.vue
+++ src/components/BooksTab.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="books-tab flex justify-center">
-    <q-scroll-area class="full-width q-px-lg" style="height: 70vh">
+    <div class="full-width">
+      <div class="row justify-center q-mb-md">
+        <div class="col col-xs-12 col-md-9">
+          <Search
+            @search="onSearch"
+            :filter-value="state.filter" @filterinput="onFilterChange"
+          />
+        </div>
+      </div>
+    </div>
+    <q-scroll-area class="full-width q-px-lg books" style="height: calc(65vh - 40px);">
       <div class="row">
         <!-- todo: Implement something like Google Keep layout -->
-        <div class="col-xs-12 col-sm-6 col-md-3 q-pa-sm" v-for="book in books" :key="book.created_at">
+        <div class="col-xs-12 col-sm-6 col-md-3 q-pa-sm" v-for="book in state.searchBooks" :key="book.created_at">
           <BookCard :book="book"/>
         </div>
       </div>
@@ -13,27 +23,83 @@
 
 <script>
 import {useStore} from "vuex";
-import {computed} from "vue";
+import {computed, reactive, onMounted} from "vue";
 import BookCard from "components/BookCard";
+import Search from "components/Search";
 
 export default {
   name: 'BooksTab',
-  components: {BookCard},
+  components: {BookCard, Search},
   setup() {
+    const state = reactive({
+      filter: 'everything',
+      searchBooks: [],
+      searchString: '',
+    });
+
     const store = useStore();
-    store.dispatch('book/getBooks');
-    return {
-      books: computed(() => store.state.book.books),
+
+    onMounted(() => store.dispatch('book/getBooks').then(res => state.searchBooks = res));
+
+    const storeBooks = computed(() => store.state.book.books);
+
+    const filterBooks = () => {
+      if (state.searchString !== '') {
+        const regex = new RegExp(state.searchString, "gi");
+        let searchBooks = store.state.book.books;
+
+        switch(state.filter) {
+          case 'everything': searchBooks = searchBooks.filter(b => {
+            const authorsNames = b.authors.map(a => a.name).join(', ');
+            const topicsNames = b.topics.map(t => t.name).join(', ');
+            return regex.test(b.title) || regex.test(authorsNames) || regex.test(topicsNames);
+          }); break;
+          case 'title': searchBooks = searchBooks.filter(b => regex.test(b.title)); break;
+          case 'author': searchBooks = searchBooks.filter(b => {
+            const authorsNames = b.authors.map(a => a.name).join(', ');
+            return regex.test(authorsNames);
+          }); break;
+          case 'topic': searchBooks = searchBooks.filter(b => {
+            const topicsNames = b.topics.map(t => t.name).join(', ');
+            return regex.test(topicsNames);
+          }); break;
+          default: break;
+        }
+        state.searchBooks = searchBooks;
+      } else state.searchBooks = store.state.book.books;
     }
+
+    const onFilterChange = (value) => {
+      state.filter = value;
+      filterBooks();
+    };
+
+    const onSearch = (value) => {
+      state.searchString = value;
+      filterBooks();
+    };
+
+    return {
+      state,
+      storeBooks,
+      filterBooks,
+      onFilterChange,
+      onSearch,
+    }
+  },
+  watch: {
+    storeBooks: {
+      handler() {
+        this.filterBooks();
+      },
+      deep: true,
+    },
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.place-card {
-  &:hover {
-    box-shadow: 1px 4px 10px #333333;
-    -webkit-transition: box-shadow .1s ease-in;
-  }
+.books {
+  height: calc(70vh - 50px);
 }
 </style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,73 +1,70 @@
 <template>
-  <q-btn-dropdown color="" label="Filter" dropdown-icon="category" class="q-mr-sm">
-    <q-list class="bg-brown-1 ">
-      <q-item
-        v-for="filter in filters" :key="filter"
-        clickable v-close-popup @click="onFilterChange(filter)"
-      >
-        <q-item-section>
-          <q-item-label :class="currentFilter === filter && 'text-bold'">
-            By <span class="text-capitalize">{{ filter }}</span>
-          </q-item-label>
-        </q-item-section>
-      </q-item>
-    </q-list>
-  </q-btn-dropdown>
-  <q-input
-    dark dense standout v-model="searchString"
-    @keypress.enter="onSearch"
-    placeholder="Search book..."
-    class="q-mr-md"
-  >
-    <template v-slot:append>
-      <q-icon v-if="searchString === ''" name="search" />
-      <q-icon v-else name="clear" class="cursor-pointer" @click="searchString = ''" />
-    </template>
-  </q-input>
-
+  <div class="row justify-center">
+    <q-btn-dropdown text-color="white" label="Filter" dropdown-icon="category" class="q-mr-sm q-mb-sm" style="width: 110px;">
+      <q-list class="bg-brown-1 ">
+        <q-item
+          v-for="filter in filters" :key="filter"
+          clickable v-close-popup @click="onFilterChange(filter)"
+        >
+          <q-item-section>
+            <q-item-label :class="filterValue === filter && 'text-bold'">
+              By <span class="text-capitalize">{{ filter }}</span>
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
+    <q-input
+      dark dense standout v-model="searchString"
+      placeholder="Search book..." @keypress.stop.enter="$emit('search', searchString)"
+      class="search-input"
+    >
+      <template v-slot:append>
+        <q-icon v-if="searchString === ''" name="search" />
+        <q-icon v-else name="clear" class="cursor-pointer" @click="onClear" />
+      </template>
+    </q-input>
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import {ref} from 'vue';
 
 export default {
   name: 'Search',
-  setup() {
+  props: ['filterValue'],
+  emits: ['filterinput', 'search'],
+  setup(props, {emit}) {
     const searchString = ref('');
-    const currentFilter = ref('everything');
-
     const filters = ['everything', 'title', 'author', 'topic'];
 
     const onFilterChange = (filter) => {
-      if (currentFilter.value === filter) return;
-
-      currentFilter.value = filter;
-      switch(filter) {
-        case 'everything': break;
-        case 'title': break;
-        case 'author': break;
-        case 'topic': break;
-        default: break;
-      }
+      if (props.filterValue === filter) return;
+      emit('filterinput', filter);
     }
 
-    const onSearch = () => {
-      if (searchString.value) {
-        console.log('Search');
-      }
+    const onClear = () => {
+      searchString.value = '';
+      emit('search', '');
     }
 
     return {
       searchString,
-      currentFilter,
-      onSearch,
+      filters,
       onFilterChange,
-      filters
+      onClear,
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.search-input {
+  width: 40%;
+}
+@media screen and (max-width: 553px) {
+  .search-input {
+    width: calc(100% - 130px) !important;
+  }
+}
 </style>
